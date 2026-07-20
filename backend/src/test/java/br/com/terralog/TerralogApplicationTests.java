753326e00a5f53266b59c428f6116b58061df7ff
Bruno Estevam <bruno.estevam@outlook.com.br>
2026-07-20 17:20:33 -0300

    build: initialize Spring Boot backend

---
+++ backend/src/test/java/br/com/terralog/TerralogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.terralog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerralogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
