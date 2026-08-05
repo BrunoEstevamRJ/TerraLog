8ea1110f6c7e285746fb5de910fc878ecd4c0701
Bruno Estevam <bruno.estevam@outlook.com.br>
2026-08-05 19:56:49 -0300

    feat(domain): create Sondagem aggregate root

---
+++ backend/src/main/java/br/com/terralog/domain/geotecnia/entity/Sondagem.java
@@ -0,0 +1,14 @@
+package br.com.terralog.domain.geotecnia.entity;
+
+import java.time.LocalDate;
+
+public class Sondagem {
+
+    private Long id;
+    private String codigo;
+    private String cliente;
+    private String obra;
+    private String endereco;
+    private LocalDate data;
+
+}
